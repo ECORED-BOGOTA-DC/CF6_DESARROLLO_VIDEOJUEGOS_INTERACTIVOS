0a26d3821f326da4cccd0667c896f27e7be1e1cd
payepesch <maechavarriaor@gmail.com>
2023-04-24 11:52:25 -0500

    Ajuste introduccion

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,7 +8,14 @@
         span
           i.fas.fa-info
       h1 Introducción
+
+    p A continuación se explican conceptos básicos para el diseño de videojuegos, programación de comportamientos básicos y construcción de prototipos. Estos conocimientos serán útiles para planear videojuegos, de acuerdo con procedimientos y requisitos técnicos a partir de los requerimientos, permitiendo así, esquematizar el prototipo físico de las mecánicas del videojuego de acuerdo al concepto.
     
+    .row.my-5(data-aos="fade-zoom")
+      figure
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/11111111" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
 </template>
 
 <script>
